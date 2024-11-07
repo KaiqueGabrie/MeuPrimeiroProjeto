f37fe1ebee7362c205f7bdc08b8e758806200c55
Kaique Gabriel <gkaique39@gmail.com>
2024-11-06 20:27:43 -0400

    MeuPrimeiroProjeto

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Meu primeiro projeto no github.");
+        }
+    }
+}
